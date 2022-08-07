b2c1d2d22d1f03b6fd0019f5e98de3e224b33602
tatl9r <e.shuvaeva@outlook.com>
2022-08-07 20:26:28 +0300

    Initial commit

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+class ApiTest {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()// Предусловия
+                .baseUri("https://postman-echo.com")
+                .body("Good evening!")// отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Good evening!"))
+        ;
+    }
+}
